56ea6a2c53b28f535486ab98be72b07979de3447
Dillon <dillon@sixmap.io>
2023-07-12 16:06:00 -0400

    initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import { v4 as uuid } from "uuid";
+import styled from "@emotion/styled";
+import { AddInput } from "./components/AddInput";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { Header } from "./components/Header";
+
+const Wrapper = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: 300,
+});
+
+/**
+* This is the initial todo state.
+*/
+const initialData: Todo[] = [
+  {
+    id: uuid(),
+    label: "Buy groceries",
+    checked: false,
+  },
+  {
+    id: uuid(),
+    label: "Reboot computer",
+    checked: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(initialData);
+
+  const addTodo = useCallback((label: string) => {
+    setTodos((prev) => [
+      {
+        id: uuid(),
+        label,
+        checked: false,
+      },
+      ...prev,
+    ]);
+  }, []);
+
+  const handleChange = useCallback((checked: boolean) => {
+    // handle the check/uncheck logic
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header>Todo List</Header>
+      <AddInput onAdd={addTodo} />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem {...todo} onChange={handleChange} />
+        ))}
+      </TodoList>
+    </Wrapper>
   );
 }
 
